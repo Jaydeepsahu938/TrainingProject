d7bcb9a83856f6887287e19280bc67d9e18bb9ba
jaydeep sahu <jaydeepsahu.ark@gmail.com>
2023-04-26 22:21:47 +0530

    first commit

--- app/src/main/java/com/example/traineeingproject/BMIApp.kt
+++ app/src/main/java/com/example/traineeingproject/BMIApp.kt
@@ -10,37 +10,32 @@ class BMIApp : AppCompatActivity() {
     private lateinit var binding: ActivityBmiappBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityBmiappBinding.inflate(layoutInflater)
+        binding = ActivityBmiappBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
     }
 
     fun calculate(view: View) {
-        val weight:Double
-        val height:Double
-        val myBMI:Float
+        val weight: Double
+        val height: Double
+        val myBMI: Float
 
-        weight=binding.weightTextView.text.toString().toDouble()
-        height=binding.heightTextView.text.toString().toDouble()
+        weight = binding.weightTextView.text.toString().toDouble()
+        height = binding.heightTextView.text.toString().toDouble()
 
-       myBMI=calculateBMI(weight,height)
-        binding.bmiTextView.text=myBMI.toString()
+        myBMI = calculateBMI(weight, height)
+        binding.bmiTextView.text = myBMI.toString()
 
-        if(myBMI<18.5)
-            binding.imageView.setImageResource(R.drawable.underweight)
-        else if(myBMI>18.5 && myBMI<24.9)
-            binding.imageView.setImageResource(R.drawable.healthy)
-        else if(myBMI>24.9 && myBMI<29.9)
-            binding.imageView.setImageResource(R.drawable.overweight)
-        else
-            binding.imageView.setImageResource(R.drawable.obesity)
+        if (myBMI < 18.5) binding.imageView.setImageResource(R.drawable.underweight)
+        else if (myBMI > 18.5 && myBMI < 24.9) binding.imageView.setImageResource(R.drawable.healthy)
+        else if (myBMI > 24.9 && myBMI < 29.9) binding.imageView.setImageResource(R.drawable.overweight)
+        else binding.imageView.setImageResource(R.drawable.obesity)
 
     }
 
-    fun calculateBMI(weight:Double,height:Double):Float
-    {
-        var myBMI:Float
-        myBMI=((weight*0.45)/((height*0.025)*(height*0.025))).toFloat()
+    fun calculateBMI(weight: Double, height: Double): Float {
+        var myBMI: Float
+        myBMI = ((weight * 0.45) / ((height * 0.025) * (height * 0.025))).toFloat()
         return myBMI
     }
 }
